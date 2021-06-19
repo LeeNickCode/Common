8ddf5ad8a1a7f077ca38853b95dcaf4153da3456
nicklee <lixianzhongim@gmail.com>
2021-06-19 17:08:51 +0800

    修改主moudle对子moudle的依赖

---
+++ moudle_common/src/main/java/com/leenick/common/MainActivity.java
@@ -0,0 +1,17 @@
+package com.leenick.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/test/activity")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
